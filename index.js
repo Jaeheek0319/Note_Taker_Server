993618a8d373ef759bed5d9c601755906273aabd
Jaehee Kim <jaeheekim@Jaehees-MBP.san.rr.com>
2022-03-27 12:02:56 -0700

    d

---
+++ index.js
@@ -0,0 +1,122 @@
+const addNote = document.querySelector("#addnote");
+const remove = document.querySelector(".delete");
+const update = document.querySelector(".updates");
+const notes = document.querySelector(".notes");
+const textArea = document.querySelector(".texts");
+const title = document.querySelector('.titles');
+
+const getNotes = () => {
+    fetch('http://localhost:3001/notes')
+        .then((res) => res.json())
+        .then((data) => {
+            console.log("data:", data);
+            let notetemplate = ""
+            for (let index = 0; index < data.length; index++) {
+                notetemplate += `
+            <div class="fullNote">
+            <div class="title"> <p> ${data[index].title} </p></div>
+            <div class="text"> <p> ${data[index].text} </p></div>
+                <!-- id = ${data[index].id} -->
+            <button  class="update" data=${data[index].id}> Update </button>
+            <button class="delete" data=${data[index].id}> Delete </button>
+            </div>
+            `
+            }
+            notes.innerHTML = notetemplate
+        });
+};
+
+getNotes();
+
+addNote.addEventListener("click", (event) => {
+    var myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    var text = textArea.value;
+    console.log(title);
+    var titleText = title.value;
+    var raw = JSON.stringify({
+        text: text,
+        title: titleText
+    });
+
+    var requestOptions = {
+        method: 'POST',
+        headers: myHeaders,
+        body: raw,
+        redirect: 'follow'
+    };
+
+    fetch("http://localhost:3001/notes", requestOptions)
+        .then(response => response.text())
+        .then(result => {
+            console.log(result)
+            getNotes();
+        })
+        .catch(error => console.log('error', error));
+});
+
+notes.addEventListener("click", (event) => {
+    if (event.target.className.includes("delete")) {
+        var requestOptions = {
+            method: 'DELETE',
+            redirect: 'follow'
+        };
+
+        const id = event.target.getAttribute("data")
+
+        fetch(`http://localhost:3001/notes/${id}`, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(result)
+                getNotes();
+            }).catch(error => console.log('error', error));
+    }
+});
+
+notes.addEventListener("click", (event) => {
+    if (event.target.className.includes("update")) {
+
+        var text = textArea.value;
+        var titleText = title.value;
+        var raw = JSON.stringify({
+            text: text,
+            title: titleText
+        });
+
+        console.log('********', raw);
+        var myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+        var requestOptions = {
+            method: 'PUT',
+            headers: myHeaders,
+            body: raw,
+            redirect: 'follow'
+        };
+
+        const id = event.target.getAttribute("data")
+
+        console.log("*********", id);
+
+
+        fetch(`http://localhost:3001/notes/${id}`, requestOptions)
+            .then(response => response.text())
+            .then(result => {
+                console.log(result)
+                getNotes();
+            }).catch(error => console.log('error', error));
+    }
+
+
+});
+
+// update.addEventListener("click", (event) => {
+//     fetch('https://localhost:3001/notes')
+//         .then((res) => res.json())
+//         .then((data) => {
+//             console.log("data:", data);
+//             console.log("ids:", data.id)
+
+
+//         });
+
+// });
---
+++ server.js
@@ -0,0 +1,164 @@
+const express = require("express");
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+const path = require('path');
+const fs = require('fs')
+const PORT = 3001;
+const { v4: uuidv4 } = require('uuid');
+const cors = require('cors');
+
+const app = express();
+
+app.use(cors())
+
+//body parsers
+app.use(express.json()); //configure request to accept json
+app.use(express.urlencoded({extended:true})) // configure request to accept arrays and string
+
+app.get("/", (req, res) => {
+    console.log(__dirname);
+    console.log(path.join(__dirname,'index.html'));
+    res.sendFile(path.join(__dirname,'index.html'), (err) => {
+        if (err) throw err;
+
+        console.log("successs");
+    })
+});
+
+app.get("/notes", (req,res) => {
+    // read notes.json
+    fs.readFile(path.join(__dirname,'note.json'), 'utf8',(err,data) =>{
+        if (err) throw err;
+
+        console.log(data);
+
+        //respond notes
+        res.json(JSON.parse(data));
+    });
+
+});
+
+app.post("/notes", (req,res) => {
+
+    //gte the new note req.body
+    const newNote = req.body;
+
+    newNote.id = uuidv4();
+    
+    //read notes js
+    fs.readFile(path.join(__dirname,'note.json'), 'utf8',(err,data) =>{
+        if (err) throw err;
+
+        console.log(data);
+
+        const UpdateNote = JSON.parse(data)
+
+        UpdateNote.push(newNote);
+
+        fs.writeFile(path.join(__dirname,'note.json'), JSON.stringify(UpdateNote), 'utf8', () => {
+            res.json(newNote);
+        }) 
+    });
+    //add to the notes array
+
+    //write it back
+});
+
+// app.delete("/notes", (req,res) => {
+// //Read note js
+//     fs.readFile(path.join(__dirname,'note.json'), 'utf8',(err,data) =>{
+//         if (err) throw err;
+
+//         console.log(data);
+
+//         const NoNote = null
+
+//         fs.writeFile(path.join(__dirname,'note.json'), JSON.stringify(UpdateNote), 'utf8', () => {
+//             res.json(newNote);
+//         }) 
+//     });
+// //delete note
+
+
+// });
+
+app.delete("/notes/:id", (req,res) => {
+    //get old note
+    const id = req.params.id
+    //Read note js
+        fs.readFile(path.join(__dirname,'note.json'), 'utf8',(err,data) =>{
+            if (err) throw err;
+    
+            console.log(data);
+    
+            const UpdateNote = JSON.parse(data);
+    
+            const filteredNotes = UpdateNote.filter((note) => note.id !== id);
+    
+            fs.writeFile(path.join(__dirname,'note.json'), JSON.stringify(filteredNotes), 'utf8', () => {
+                res.json(filteredNotes);
+            }) 
+        });
+});
+
+app.put("/notes/:id", (req,res) => {
+    const newNote = req.body
+    const id = req.params.id
+
+    console.log("req.body",req.body);
+    console.log("id", id);
+
+    fs.readFile(path.join(__dirname, 'note.json'), 'utf8', (err,data) => {
+        if (err) throw err;
+        
+        const UpdateNote = JSON.parse(data);
+        
+        const targetNote = UpdateNote.filter((note) => note.id === id)[0];
+        
+        console.log("targetNote before",targetNote);
+        
+        targetNote.text = newNote.text;
+        targetNote.title = newNote.title;
+
+        console.log("targetNote after",targetNote);
+
+        fs.writeFile(path.join(__dirname,'note.json'), JSON.stringify(UpdateNote), 'utf8', () => {
+            res.json(UpdateNote);
+        })
+    })
+
+})
+
+app.listen(PORT, () => {
+     console.log(`Server Listening on ${PORT} ...`);
+});
+
+
+
+// const notes = {
+//     1: {
+//       id: 1,
+//       title: 'Test Note',
+//       body: 'Test Note Object',
+//       created_at: Date.now(),
+//       }
+//     };
+    
+// app.post('/notes', notes.create);
+// app.get('/notes', notes.findAll);
+// app.delete('/notes/:noteID', notes.delete);
+
+// const createNextId = Number.parseInt(Math.max(...Object.keys(notes).map((key) => Number.parseInt(key))), 10) + 1;
+
+// const AddNote = ({
+//     title,
+//     body,
+// }) 
+
+// notes[id] = {
+//     id,
+//     title,
+//     body,
+//     created_at: Date.now(),
+// };
+// return notes[id]
